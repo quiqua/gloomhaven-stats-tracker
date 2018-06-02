00b6cb64a33359d1f453d904ecd0367c719d0953
Marcel Radischat <sabodendar@googlemail.com>
2018-06-02 23:55:54 +0200

    Replace example Spek test

--- app/src/test/kotlin/eu/quiqua/gloomhaven/statstracker/features/stats/viewModel/StatsViewModelSpek.kt
+++ app/src/test/kotlin/eu/quiqua/gloomhaven/statstracker/features/stats/viewModel/StatsViewModelSpek.kt
@@ -1,18 +1,18 @@
-package eu.quiqua.gloomhaven.statstracker
+package eu.quiqua.gloomhaven.statstracker.features.stats.viewModel
 
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions
 
-object CalculatorSpec: Spek({
+internal class StatsViewModelSpek : Spek({
     given("a calculator") {
         val calculator = "hello"
         on("addition") {
             val sum = calculator + "world"
             it("should return the result of adding the first number to the second number") {
-                assertEquals(6, sum)
+                Assertions.assertEquals(6, sum)
             }
         }
     }
