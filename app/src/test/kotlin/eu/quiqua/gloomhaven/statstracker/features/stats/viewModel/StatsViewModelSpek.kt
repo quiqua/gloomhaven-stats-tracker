e20dfe8c1f15e549a52c4b8cbd8b7ec5971a4ef6
Marcel Radischat <marcel@door2door.io>
2018-06-04 12:11:38 +0200

    Use hamkrest matchers

--- app/src/test/kotlin/eu/quiqua/gloomhaven/statstracker/features/stats/viewModel/StatsViewModelSpek.kt
+++ app/src/test/kotlin/eu/quiqua/gloomhaven/statstracker/features/stats/viewModel/StatsViewModelSpek.kt
@@ -1,12 +1,13 @@
 package eu.quiqua.gloomhaven.statstracker.features.stats.viewModel
 
+import com.natpryce.hamkrest.assertion.assert
+import com.natpryce.hamkrest.equalTo
 import eu.quiqua.gloomhaven.statstracker.app.Application
 import eu.quiqua.gloomhaven.statstracker.features.stats.viewmodel.StatsViewModel
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
-import org.junit.jupiter.api.Assertions
 
 internal class StatsViewModelSpek : Spek({
     given("an application") {
@@ -15,7 +16,7 @@ internal class StatsViewModelSpek : Spek({
             val statsViewModel = StatsViewModel(application)
             on("getStats") {
                 it("should return the defined model with its stats") {
-                    Assertions.assertEquals("hello world", statsViewModel.getStats())
+                    assert.that(statsViewModel.getStats(), equalTo("hello world"))
                 }
             }
         }
